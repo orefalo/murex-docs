23acc03ee3140b8c1efa51044bc8ef3c5966cb14
Olivier Refalo <orefalo@snapcore.com>
2023-07-07 00:25:42 +0400

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/murex-docs/",
   lang: "en-US",
   title: "",
   description: "Murex, a typed, content aware shell from the 2020s",
