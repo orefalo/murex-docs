fb365d8dd2b6e8d3ab702517fc4a7ecc38365a27
olivier <orefalo@gmail.com>
2023-07-07 14:07:19 +0400

    add search

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,26 +1,38 @@
-import { defineUserConfig } from "vuepress";
+import { defineUserConfig, Page } from "vuepress";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 const environment = process.env.NODE_ENV;
 
-let config;
+const config = {
+  base: "/murex-docs/",
+  lang: "en-US",
+  title: "",
+  description: "Murex, a typed, content aware shell from the 2020s",
+  theme,
+  plugins: [
+    searchProPlugin({
+      // index all contents
+      indexContent: true,
+      // add supports for category and tags
+      customFields: [
+        {
+          //@ts-ignore
+          getter: (page) => page.frontmatter.category,
+          formatter: "Category: $content",
+        },
+        {
+          //@ts-ignore
+          getter: (page) => page.frontmatter.tag,
+          formatter: "Tag: $content",
+        },
+      ],
+    }),
+  ],
+};
 
 if (environment === "DEV") {
-  config = {
-    base: "/",
-    lang: "en-US",
-    title: "",
-    description: "Murex, a typed, content aware shell from the 2020s",
-    theme,
-  };
-} else {
-  config = {
-    base: "/murex-docs/",
-    lang: "en-US",
-    title: "",
-    description: "Murex, a typed, content aware shell from the 2020s",
-    theme,
-  };
+  config.base = "/";
 }
-
+//@ts-ignore
 export default defineUserConfig(config);
