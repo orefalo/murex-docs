a93ad340ea2412e56dfa06d2916a518a8cc75e83
Olivier Refalo <orefalo@snapcore.com>
2023-07-17 16:43:30 +0400

    add google font

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,4 @@
-import { defineUserConfig, Page } from "vuepress";
+import { defineUserConfig } from "vuepress";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
@@ -9,6 +9,20 @@ const config = {
   lang: "en-US",
   title: "",
   description: "Murex, a typed, content aware shell from the 2020s",
+  head: [
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+  ],
   theme,
   plugins: [
     searchProPlugin({
